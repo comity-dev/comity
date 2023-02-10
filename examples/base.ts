6e95dd0db0a63782b440adeebd1fab34e83fa87b
teaishealthy <teaishealthy@protonmail.com>
2023-02-10 17:14:13 +0100

    feat!: builders

--- examples/base.ts
+++ examples/base.ts
@@ -1,20 +1,18 @@
-import { Client } from 'comity';
+import { Client, SlashCommandBuilder } from 'comity';
 import { InteractionCallbackData } from 'discord-typings';
 
 export const client = new Client('token');
 
-client.addGlobalCommand(
-    {
-        name: 'ping',
-        description: 'Pong!',
-        default_member_permissions: '0',
-    },
-    (interaction) => {
-        return {
-            type: 4,
-            data: {
-                content: 'Pong!',
-            },
-        } as InteractionCallbackData;
-    },
+client.addCommand(
+    new SlashCommandBuilder()
+        .name('ping')
+        .description('Pong!')
+        .handler((inter) => {
+            return {
+                type: 4,
+                data: {
+                    content: 'Pong!',
+                },
+            } as InteractionCallbackData;
+        }),
 );
